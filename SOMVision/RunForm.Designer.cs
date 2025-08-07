6ad088176b30dec7778f2d7f4b3940a68c72fa34
dasom <zlal324@gmail.com>
2025-08-07 16:19:44 +0900

    250807_3

--- SOMVision/RunForm.Designer.cs
+++ SOMVision/RunForm.Designer.cs
@@ -43,7 +43,7 @@
             this.btnGrab.Location = new System.Drawing.Point(11, 13);
             this.btnGrab.Margin = new System.Windows.Forms.Padding(4);
             this.btnGrab.Name = "btnGrab";
-            this.btnGrab.Size = new System.Drawing.Size(123, 107);
+            this.btnGrab.Size = new System.Drawing.Size(82, 82);
             this.btnGrab.TabIndex = 1;
             this.btnGrab.UseVisualStyleBackColor = true;
             this.btnGrab.Click += new System.EventHandler(this.btnGrab_Click);
@@ -60,10 +60,10 @@
             // 
             this.btnInsp.ImageIndex = 2;
             this.btnInsp.ImageList = this.runImageList;
-            this.btnInsp.Location = new System.Drawing.Point(273, 13);
+            this.btnInsp.Location = new System.Drawing.Point(191, 13);
             this.btnInsp.Margin = new System.Windows.Forms.Padding(4);
             this.btnInsp.Name = "btnInsp";
-            this.btnInsp.Size = new System.Drawing.Size(123, 107);
+            this.btnInsp.Size = new System.Drawing.Size(82, 82);
             this.btnInsp.TabIndex = 2;
             this.btnInsp.UseVisualStyleBackColor = true;
             this.btnInsp.Click += new System.EventHandler(this.btnInsp_Click);
@@ -72,10 +72,10 @@
             // 
             this.btnLive.ImageIndex = 1;
             this.btnLive.ImageList = this.runImageList;
-            this.btnLive.Location = new System.Drawing.Point(142, 13);
+            this.btnLive.Location = new System.Drawing.Point(100, 13);
             this.btnLive.Margin = new System.Windows.Forms.Padding(4);
             this.btnLive.Name = "btnLive";
-            this.btnLive.Size = new System.Drawing.Size(123, 107);
+            this.btnLive.Size = new System.Drawing.Size(82, 82);
             this.btnLive.TabIndex = 3;
             this.btnLive.UseVisualStyleBackColor = true;
             this.btnLive.Click += new System.EventHandler(this.btnLive_Click);
@@ -84,7 +84,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(524, 212);
+            this.ClientSize = new System.Drawing.Size(294, 108);
             this.Controls.Add(this.btnLive);
             this.Controls.Add(this.btnInsp);
             this.Controls.Add(this.btnGrab);
